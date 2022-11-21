9d048f78749c7ed7e299f4c22dbe5444347844a2
SaiVenkat12 <gsaiv1001@gmail.com>
2022-11-21 08:35:40 +0530

    [SaiVenkat]

--- num.sh
+++ num.sh
@@ -3,3 +3,12 @@
 read -p "Enter the number:" num1;
 read -p "Enter the second number:" num2;
 
+if [ $num1 -gt $num2 ]
+then
+	echo "$num1 is greater than $num2"
+elif [ $num2 -gt $num1 ]
+then
+	echo "$num2 is greater than $num1"
+else
+	echo "$num1 is equal to $num2"
+fi
